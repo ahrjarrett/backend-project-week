7bc6d50b59d1e14e4ee348dc049946b2332b8961
Andrew Jarrett <ahrjarrett@gmail.com>
2018-11-30 00:54:14 -0600

    fix production port number to 80 from 8008

--- server/src/config/prod.js
+++ server/src/config/prod.js
@@ -1,7 +1,7 @@
 const { MONGO_ADMIN, MONGO_ADMIN_PASSWORD } = process.env
 
 export const config = {
-  port: 8008,
+  port: 80,
   db: {
     url: `mongodb://${MONGO_ADMIN}:${MONGO_ADMIN_PASSWORD}@ds121814.mlab.com:21814/backend-project-week`
   },
